dc04ee31c472eed9948bf6994b288ec33de19bf6
yoshimura2004 <kksik0844@naver.com>
2025-10-20 11:17:00 +0900

    Add files via upload

--- src/main/java/kr/or/mes/dto/LotTracking2DTO.java
+++ src/main/java/kr/or/mes/dto/LotTracking2DTO.java
@@ -4,9 +4,7 @@ import java.util.Date;
 import lombok.*;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
 public class LotTracking2DTO {
     private int trackingId;         // 추적 ID
     private String lotNumber;       // LOT 번호
